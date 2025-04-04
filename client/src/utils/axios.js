1546719791704d1c6ed174f97d1de7e6de631f51
shadtorh <torhshad09@gmail.com>
2025-04-04 01:18:09 -0400

    Initial commit

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+const API = import.meta.env.VITE_API_URL;
 
 const instance = axios.create({
-	baseURL: "http://localhost:3000/api", // Make sure this is correct
+	// baseURL: "http://localhost:3000/api", // Make sure this is correct
+	baseURL: `${API}/api`, // Use the environment variable for the base URL
 	withCredentials: true, // Ensure CORS supports credentials if needed
 });
 
