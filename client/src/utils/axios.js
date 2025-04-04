cbe92a617f09d85ccf0807a01e0077753f9b0a0c
shadtorh <torhshad09@gmail.com>
2025-04-04 02:51:31 -0400

    testing

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-const API = import.meta.env.VITE_API_URL;
+const API =
+	import.meta.env.VITE_API_URL ||
+	"https://e-commerce-store-web-app.onrender.com";
 
 const instance = axios.create({
 	// baseURL: "http://localhost:3000/api", // Make sure this is correct
