2f7d52db3014366c66613275f96ed2a73ceabed4
shadtorh <torhshad09@gmail.com>
2025-04-24 21:35:48 -0400

    let's go

--- server/index.js
+++ server/index.js
@@ -53,10 +53,10 @@ app.get("/", (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
 	const __dirname = path.resolve();
-	app.use(express.static(path.join(__dirname, "/client/build")));
+	app.use(express.static(path.join(__dirname, "/client/dist")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
 	});
 }
 
