a6d30588f7d3eb6f6a546e652a5234a5adecfe56
shadtorh <torhshad09@gmail.com>
2025-04-24 21:06:05 -0400

    let's go

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-// const API =
-// 	import.meta.env.VITE_API_URL ||
-// 	"https://e-commerce-store-web-app-0xlf.onrender.com";
+const API =
+	import.meta.env.VITE_API_URL ||
+	"https://e-commerce-store-web-app-sand.vercel.app";
 
 const instance = axios.create({
-	baseURL: "http://localhost:3000/api", // Make sure this is correct
-	// baseURL: `${API}/api`, // Use the environment variable for the base URL
+	// baseURL: "http://localhost:3000/api", // Make sure this is correct
+	baseURL: `${API}/api`, // Use the environment variable for the base URL
 	withCredentials: true, // Ensure CORS supports credentials if needed
 });
 
